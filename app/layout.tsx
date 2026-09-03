0739c29b9acc0d12c03dba3f98707014f029fdfa
David Schol <d.schol@fontys.nl>
2026-09-03 14:14:21 +0000

    Header en Footer components toegevoegd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
   title: {
@@ -17,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="nl">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+        </body>
     </html>
   );
 }
