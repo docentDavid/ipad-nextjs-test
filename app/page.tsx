1ba166460918a678d6e04d6313f7bdcf632ed8e7
David Schol <d.schol@fontys.nl>
2026-09-03 12:38:37 +0000

    Fresh start

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Hallo wereld
+      <h1>Hallo wereld</h1>
+      <p>Lorem Ipsum</p>
     </div>
   );
 }
